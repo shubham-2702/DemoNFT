d0d9e7e92ed63fbfb0d30bacd390b8bd8771ccd3
shubham-2702 <71482898+shubham-2702@users.noreply.github.com>
2023-01-18 13:41:04 +0530

    Updated for Gaura

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -12,15 +12,25 @@ contract MyNFT is ERC721URIStorage , Ownable{
 
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("Blockchain Boutique","BB"){}
+    constructor() ERC721("Gaura Web Tech","GWT"){}
 
-    function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns(uint256)
+    function mintNFT(address _recipient, string memory _tokenURI) public returns(uint256)
     {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+        _mint(_recipient, newItemId);
+        _setTokenURI(newItemId, _tokenURI);
         return newItemId;
+    }
+    
+    function mintMultipleNFT(address _recipient, string memory _tokenURI, uint _amount) public returns(uint[] memory _Ids)
+    {
+        uint[] memory Ids=new uint[](_amount);
+        for(uint i=0;i<_amount;i++)
+        {
+            Ids[i]=mintNFT(_recipient,_tokenURI);
+        }
+        return Ids;
     }
     
 }
